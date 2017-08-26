33a22712d82ce50eeb255fb9f437a65aad32de26
Daniil <daniel217032001@gmail.com>
2017-08-26 17:57:45 +0300

    Add starting code

---
+++ getData.php
@@ -0,0 +1,60 @@
+<?php
+if (!$_POST['data'] == "") {
+    $data = json_decode($_POST["data"], true);
+
+    $str = file_get_contents("pages/" . $data['date'] . "_" . $data['month'] . ".php", true);
+    $str = strip_tags($str,'<h1><h2><h3><h4><h5><h6><ul><li><a><img>');
+    preg_match("'<h3>.*?Национальные.*?</h3>.*?</ul>'", $str, $strNational);
+    preg_match_all("'<ul>.*?</ul>'", $strNational[0], $strNational);
+    $strNational = preg_replace("'<li>.*?.<a'", "<li><a", $strNational[0][count($strNational[0]) - 1]); //BUGFIX
+
+    preg_match("'<h3>.*?Профессиональные.*?</h3>.*?</ul>'", $str, $strProfessional);
+    preg_match_all("'<ul>.*?</ul>'", $strProfessional[0], $strProfessional);
+    $strProfessional = preg_replace("'<li>.*?.<a'", "<li><a", $strProfessional[0][count($strProfessional[0]) - 1]); //BUGFIX
+
+    preg_match("'<h3>.*?Международные.*?</h3>.*?</ul>'", $str, $strInternational);
+    preg_match_all("'<ul>.*?</ul>'", $strInternational[0], $strInternational);
+    $strInternational = preg_replace("'<li>.*?.<a'", "<li><a", $strInternational[0][count($strInternational[0]) - 1]); //BUGFIX
+
+    preg_match("'</ul><h2>.*?События.*?</h2>.*?<h2>'", $str, $strEvents);
+    $strEvents = $strEvents[0]; //BUGFIX
+    $strEvents = preg_replace("'<h2>.*?</h2>'", "", $strEvents); // Full deleting h2 tags
+
+    $returnArray = array(
+        "strNational" => $strNational,
+        "strProfessional" => $strProfessional,
+        "strInternational" => $strInternational,
+        "strEvents" => $strEvents
+    );
+    echo json_encode($returnArray);
+} else {
+    echo "Выйди и зайди нормально";
+}
+
+
+
+//=============Old code for reforming code=================
+
+/*
+ //Удаляем head
+$str = preg_replace("'<head>.*?</head>'si", " ", $str);
+// Удаляет все элементы до блока с ссобытиями
+$str = $str = preg_replace("'<body.*?<h2>Содержание.*?</div>.*?</div>'si", "<body>", $str);
+// Удаляет все элементы после блока с ссобытиями
+$str = substr($str, 0, 0 - (strlen($str) - strpos($str, "<h2><span class=\"mw-headline\" id=\".D0.A1.D0.BC._.D1.82.D0.B0.D0.BA.D0.B6.D0.B5\">")));
+// Делаем нерабочие ссылки на статьи в википедии рабочими
+$str = str_replace("href=\"/wiki", "href=\"https://ru.wikipedia.org/wiki", $str);
+$str = str_replace("href=\"/w/", "href=\"https://ru.wikipedia.org/w/", $str);
+// Удаляем ссылки для правки статей
+$str = preg_replace("'<span class=\"mw-editsection\">.*?</span>.*?</span>.*?</span>'si", " ", $str);
+// Удаляем  лишние пробелы через <p>
+$str = preg_replace("'<p>.*?</p>'si", " ", $str);
+// Удаляем [справка]
+$str = preg_replace("'<sup.*?>.*?</sup>'si", " ", $str);
+// Удаляем блоки "См. также"
+$str = preg_replace("'<div class=\"dablink\">.*?</div>'si", " ", $str);
+// Удаляем блоки с фотографиями
+$str = preg_replace("'<div class=\"thumb tright\">.*?</div>.*?</div>'si", " ", $str);
+//Делаем файл в одну строку чтобы можно было искать регуляркой по несколько строк.
+$str = str_replace(array("\r", "\n"), "", $str);
+ */
---
+++ index.php
@@ -0,0 +1,83 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Date Parser</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="menu clearfix">
+    <div class="menu-item menu-item__active">
+        Сегодняшние события
+    </div>
+    <div class="menu-item menu-item__disabled">
+        Выбрать другую дату
+    </div>
+    <div class="menu-item menu-item__disabled">
+        Рассылка
+    </div>
+</div>
+
+<div class="wrapper">
+    <div class="calendar clearfix">
+        <div class="calendar-head">Календарь</div>
+        <div class="calendar-daysOfWeek">
+            <div class="daysOfWeek-day">пн</div>
+            <div class="daysOfWeek-day">вт</div>
+            <div class="daysOfWeek-day">ср</div>
+            <div class="daysOfWeek-day">чт</div>
+            <div class="daysOfWeek-day">пт</div>
+            <div class="daysOfWeek-day">сб</div>
+            <div class="daysOfWeek-day">вс</div>
+        </div>
+        <div class="calendar-body">
+            <div class="calendar-date">1</div>
+            <div class="calendar-date">2</div>
+            <div class="calendar-date">3</div>
+            <div class="calendar-date">4</div>
+            <div class="calendar-date">5</div>
+            <div class="calendar-date">6</div>
+            <div class="calendar-date">7</div>
+            <div class="calendar-date">8</div>
+            <div class="calendar-date">9</div>
+            <div class="calendar-date">10</div>
+            <div class="calendar-date">11</div>
+            <div class="calendar-date">12</div>
+            <div class="calendar-date">13</div>
+            <div class="calendar-date">14</div>
+            <div class="calendar-date">15</div>
+            <div class="calendar-date">16</div>
+            <div class="calendar-date">17</div>
+            <div class="calendar-date">18</div>
+            <div class="calendar-date">19</div>
+            <div class="calendar-date">20</div>
+            <div class="calendar-date">21</div>
+            <div class="calendar-date">22</div>
+            <div class="calendar-date">23</div>
+            <div class="calendar-date">24</div>
+            <div class="calendar-date">25</div>
+            <div class="calendar-date">26</div>
+            <div class="calendar-date">27</div>
+            <div class="calendar-date">28</div>
+            <div class="calendar-date">29</div>
+            <div class="calendar-date">30</div>
+            <div class="calendar-date">31</div>
+        </div>
+    </div>
+    <div class="events">
+        <div class="events-national"></div>
+        <div class="events-professional"></div>
+        <div class="events-international"></div>
+        <div class="events-history">
+            <h2>Исторические события</h2>
+        </div>
+    </div>
+</div>
+<script type="text/javascript" src="jquery-3.2.1.min.js"></script>
+<script type="text/javascript" src="scripts.js"></script>
+</body>
+</html>
